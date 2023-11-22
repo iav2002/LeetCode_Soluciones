4c006de4884db4f4e7193eea4cd2e8bc63381780
Ignacio Alarcon Varela <ignacioalarcon2002@gmail.com>
2023-11-22 23:57:07 +0000

    Sliding Window

--- patterns_examples/SlidingWindow.py
+++ patterns_examples/SlidingWindow.py
@@ -15,3 +15,47 @@ def find_length(nums, k):
         ans = max(ans, right - left + 1)
     
     return ans
+
+# finding the subArray of products that is less than K
+def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
+    if k <= 1:
+        return 0
+
+    ans = left = 0
+    curr = 1
+
+    for right in range(len(nums)):
+        curr *= nums[right]
+        while curr >= k:
+            curr //= nums[left]
+            left += 1
+        ans += right - left + 1
+
+    return ans
+
+#SlidingWindowFixed
+def find_best_subarray(nums, k):
+    curr = 0
+    for i in range(k):
+        curr += nums[i]
+    
+    ans = curr
+    for i in range(k, len(nums)):
+        curr += nums[i] - nums[i - k]
+        ans = max(ans, curr)
+    
+    return ans
+
+# 643. Maximum Average Subarray I, only divide ans in K
+# You are given an integer array nums consisting of n elements, and an integer k.
+
+# Find a contiguous subarray whose length is equal to k that has the maximum average value and return this value. Any answer with a calculation error less than 10-5 will be accepted.
+# Example 1:
+
+# Input: nums = [1,12,-5,-6,50,3], k = 4
+# Output: 12.75000
+# Explanation: Maximum average is (12 - 5 - 6 + 50) / 4 = 51 / 4 = 12.75
+# Example 2:
+
+# Input: nums = [5], k = 1
+# Output: 5.00000
--- patterns_examples/twoPointers.py
+++ patterns_examples/twoPointers.py
@@ -1,50 +1,59 @@
-# def check_for_target(nums, target):
-#     left = 0
-#     right = len(nums) - 1
+def check_for_target(nums, target):
+    left = 0
+    right = len(nums) - 1
 
-#     while left < right:
-#         # curr is the current sum
-#         if nums[left] + nums[right] == target:
-#             return True
-#         elif nums[left] + nums[right]> target:
-#             right -= 1
-#         elif nums[left] + nums[right]:
-#             left += 1
+    while left < right:
+        # curr is the current sum
+        if nums[left] + nums[right] == target:
+            return True
+        elif nums[left] + nums[right]> target:
+            right -= 1
+        elif nums[left] + nums[right]:
+            left += 1
     
-#     return False
+    return False
 
 
-# ## Combining two sorted arrays in O(n) instead of using 0(n log N)
-# def combine(arr1, arr2):
-#     # ans is the answer
-#     ans = []
-#     i = j = 0
-#     while i < len(arr1) and j < len(arr2):
-#         if arr1[i] < arr2[j]:
-#             ans.append(arr1[i])
-#             i += 1
-#         else:
-#             ans.append(arr2[j])
-#             j += 1
-    
-#     while i < len(arr1):
-#         ans.append(arr1[i])
-#         i += 1
-    
-#     while j < len(arr2):
-#         ans.append(arr2[j])
-#         j += 1
-    
-#     return ans
-
-def sortedSquares(nums: list[int]):
-    numero = nums[0]
+## Combining two sorted arrays in O(n) instead of using 0(n log N)
+def combine(arr1, arr2):
+    # ans is the answer
     ans = []
-    for i in nums:
-        square =  numero * numero
-        ans.append(square)
-
+    i = j = 0
+    while i < len(arr1) and j < len(arr2):
+        if arr1[i] < arr2[j]:
+            ans.append(arr1[i])
+            i += 1
+        else:
+            ans.append(arr2[j])
+            j += 1
+    
+    while i < len(arr1):
+        ans.append(arr1[i])
+        i += 1
+    
+    while j < len(arr2):
+        ans.append(arr2[j])
+        j += 1
+    
     return ans
+
+# Saca
+# r los cuadrados de cada item de la array y ordenarlo
+def sortedSquares(nums: list[int]):
+    l = 0
+    r = len(nums) - 1
+    ans = []
+
+    while l <= r:
+        x = pow(nums[l], 2) 
+        y = pow(nums[r], 2)
+        if x > y:
+            ans.append(x)
+            l = l + 1 
+        else:
+            ans.append(y)
+            r = r - 1
+    return ans[::-1]
 
 nums = [2,2,3,4,5,6]
 
